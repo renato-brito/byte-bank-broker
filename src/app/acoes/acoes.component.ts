46cc058bd6d0214cfad2bf03a29926829aea8e46
Renato Santana Brito <britotecst@outlook.com>
2022-06-19 13:13:20 -0300

    finished RxJS

--- src/app/acoes/acoes.component.ts
+++ src/app/acoes/acoes.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { merge, Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
+import { AcoesService } from './acoes.service';
+import { Acoes } from './modelo/acoes';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
+
+const ESPERA_DIGITACAO = 300;
 
 @Component({
   selector: 'app-acoes',
@@ -8,6 +14,42 @@ import { FormControl } from '@angular/forms';
 })
 export class AcoesComponent {
   acoesInput = new FormControl();
+  // acoes$ = this.acoesService.getAcoes();
 
-  constructor() {}
+  todasAcoes$ = this.acoesService.getAcoes().pipe(
+    tap(() => {
+      console.log('Fluxo Inicial');
+    })
+  );
+
+  filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITACAO),
+    tap(() => {
+      console.log('Fluxo do filtro');
+    }),
+    tap(console.log),
+    filter((valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
+    tap(console.log)
+  );
+
+  // acoes$ = this.acoesInput.valueChanges.pipe(
+  //   tap(console.log),
+  //   switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
+  //   tap(console.log)
+  // );
+  acoes$ = merge(this.todasAcoes$, this.filtroPeloInput$);
+
+  constructor(private acoesService: AcoesService) {}
+
+  // ngOnInit(): void {
+  //   this.subscription = this.acoesService.getAcoes().subscribe((acoes) => {
+  //     this.acoes = acoes;
+  //   });
+  // }
+
+  // ngOnDestroy(): void {
+  //   this.subscription.unsubscribe();
+  // }
 }
